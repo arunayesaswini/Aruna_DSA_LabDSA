0a7a6b148d3734942462be4e3a3dd8e26c0d9a6c
arunayesaswini <127669982+arunayesaswini@users.noreply.github.com>
2023-04-26 00:29:48 +0530

    Add files via upload

---
+++ GL_LABDSA_FindPairSumBST/src/com/greatleaning/model/Node.java
@@ -0,0 +1,14 @@
+package com.greatleaning.model;
+
+public class Node {
+	Node left;
+	int data;
+	Node right;
+
+	public Node(int data) {
+		left = null;
+		this.data = data;
+		right = null;
+	}
+
+}
